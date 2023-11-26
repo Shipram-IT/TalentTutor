1953f8314cb7c48f16386b479ac0268073c97404
mosuromayowa <mosuromayowa@gmail.com>
2023-11-26 10:34:02 -0500

    loop through employees, managers and quiz master to print details

--- src/Company.java
+++ src/Company.java
@@ -3,16 +3,42 @@ import java.util.List;
 
 public class Company {
     private List<Employee> employeeList = new ArrayList<>();
-    private List<QuizMaster> quizMaster = new ArrayList<>();
+    private List<QuizMaster> quizMasterList = new ArrayList<>();
+
+    private List<Manager> managerList = new ArrayList<>();
 
     public void addEmployee(String name, EmployeeType type) {
         Employee newEmployee = new Employee(name,type);
         employeeList.add(newEmployee);
     }
 
-    public void addQuizMaster (String name) {
+    public void addQuizMaster(String name) {
         QuizMaster newQuizMaster = new QuizMaster(name);
-        quizMaster.add(newQuizMaster);
+        quizMasterList.add(newQuizMaster);
     }
+
+    public void addManger (String name) {
+        Manager newManager = new Manager(name);
+        managerList.add(newManager);
+    }
+
+    public void printEmploeeDetails() {
+        System.out.println("Employees: ");
+        for (Employee employee : employeeList) {
+            System.out.println("Employee Details: " + employee.getName() + " " + employee.getType());
+        }
+
+        System.out.println("\nQuizMaster: ");
+        for (QuizMaster quizMaster: quizMasterList) {
+            System.out.println("Quiz Details: " + quizMaster.getName() + " " + quizMaster.getType());
+
+        }
+
+        for (Manager manager: managerList) {
+            System.out.println();
+        }
+    }
+
+
 
 }
