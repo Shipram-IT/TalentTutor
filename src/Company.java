999e0b4c3cea4b090e67b75f2181ea90871af75b
mosuromayowa <mosuromayowa@gmail.com>
2023-11-26 09:47:55 -0500

    created the methods to add quizmaster and employee

--- src/Company.java
+++ src/Company.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Company {
+    private List<Employee> employeeList = new ArrayList<>();
+    private List<QuizMaster> quizMaster = new ArrayList<>();
+
+    public void addEmployee(String name, EmployeeType type) {
+        Employee newEmployee = new Employee(name,type);
+        employeeList.add(newEmployee);
+    }
+
+    public void addQuizMaster (String name) {
+        QuizMaster newQuizMaster = new QuizMaster(name);
+        quizMaster.add(newQuizMaster);
+    }
+
 }
