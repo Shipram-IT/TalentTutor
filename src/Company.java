98b1dd240a9c77dbad55a5a55eeb602de1f56e7a
PaulomK <paulo.massao.kawakami@gmail.com>
2023-11-26 22:57:49 -0500

    Unify collection to the Parent Employee type. - set company constructor template - remove methods that could cause conflict as the other lists are removed

--- src/Company.java
+++ src/Company.java
@@ -1,62 +1,17 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class Company {
-    private static List<Employee> employeeList = new ArrayList<>();
-    private static List<QuizMaster> quizMasterList = new ArrayList<>();
+    //Fields
+    private ArrayList<Employee> employeeList;
 
-    private List<Manager> managerList = new ArrayList<>();
-
-    private List<RegularEmployee> regularEmployeesList = new ArrayList<>();
-
-
-    public void addQuizMaster(QuizMaster quizMaster) {
-        quizMasterList.add(quizMaster);
+    //Constructor
+    public Company(){
+        this.employeeList = new ArrayList<Employee>();
     }
 
-
-    public void addManger (String name) {
-        Manager newManager = new Manager(name);
-        managerList.add(newManager);
+    //Methods
+    public void addEmployee(Employee employee) {
+        employeeList.add(employee);
     }
-
-    public void addRegularEmployee (String name, Department department) {
-        RegularEmployee newregularEmployee = new RegularEmployee(name, department);
-        regularEmployeesList.add(newregularEmployee);
-    }
-
-    public static void addQuizMaster(String name) {
-        QuizMaster newQuizMaster = new QuizMaster(name);
-        quizMasterList.add(newQuizMaster);
-    }
-
-    public static void addEmployee(String name, EmployeeType type) {
-        Employee newEmployee = new Employee(name,type);
-        employeeList.add(newEmployee);
-
-    }
-
-    public void printEmploeeDetails() {
-        System.out.println("Employees: ");
-        for (Employee employee : employeeList) {
-            System.out.println("Employee Details: " + employee.getName() + " " + employee.getType());
-        }
-
-        System.out.println("\nQuizMaster: ");
-        for (QuizMaster quizMaster: quizMasterList) {
-            System.out.println("Quiz Details: " + quizMaster.getName() + " " + quizMaster.getType());
-
-        }
-
-        for (Manager manager: managerList) {
-            System.out.println("Manager Details: " + manager.getName() + " " + manager.getType());
-        }
-
-        for (RegularEmployee regularEmployee: regularEmployeesList) {
-            System.out.println("Regular Employee details: " + regularEmployee.getName() + " " + regularEmployee.getDepartment());
-        }
-    }
-
-
 
 }
