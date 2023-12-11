5e1720073cf57e2921a2951fc34e34dfe5e803c5
rafaeatul-kabir <rafaeatul@gmail.com>
2023-12-11 01:16:45 -0500

    implemented menu class and updated main and menu class

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
         //initialize program
         Company myCompany;
         try {
@@ -16,15 +13,12 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
         //testing set a manager
         Manager m1 = new Manager("Johnny The Tester");
         myCompany.addEmployee(m1);
-
         //testing employee hiring
         Employee e1 = new QuizMaster("Jerry the QuizMaster");
         m1.hireEmployee(myCompany, e1);
-
         //show employees in list
         for (Employee e : myCompany.getEmployees()) {
             System.out.println(e.getName() + " " + e.getType());} //shows like this while toString() not implemented
@@ -48,9 +42,15 @@ public class Main {
         for (int i = 0; i < employeesList.size(); i++) {
             System.out.printf("[%d] - %s - %s\n", i, employeesList.get(i).getName(), employeesList.get(i).getType());
         }
+
+
+        Menu mainMenu = new Menu(myCompany) ;
+
+        while (true) {
+            mainMenu.displayMenu();
+            int choice = mainMenu.scanner.nextInt();
+            mainMenu.processChoice(choice);
+        }
     }
-
-
-
-
 }
+
