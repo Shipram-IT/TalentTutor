58c5edddea7eb5dd448a4550b23beaf8cc89b704
Shivani Sehgal <sehgalshivani07@gmail.com>
2023-12-11 18:55:20 -0500

    Added MCQ question class

--- src/classes/Question/MCQQuestion.java
+++ src/classes/Question/MCQQuestion.java
@@ -1,13 +1,18 @@
+package classes.Question;
+
+import classes.Question.Question;
+import enums.Department;
+
 import java.util.List;
 
 /**
- * Concrete class for Multiple Choice Question
+ * Concrete class for Multiple Choice classes.Question.Question
  */
 public class MCQQuestion extends Question {
     private final List<String> options;
 
     /**
-     * Constructor for MCQQuestion.
+     * Constructor for classes.Question.MCQQuestion.
      *
      * @param questionBody    The body of the question.
      * @param answer          The answer to the question.
@@ -15,9 +20,9 @@ public class MCQQuestion extends Question {
      * @param department      The department associated with the question.
      * @param options         The options for the MCQ question.
      */
-    public MCQQuestion(String questionBody, String answer, int difficultyLevel, Department department,
+    public MCQQuestion(String topic, String questionBody, String answer, int difficultyLevel, Department department,
                        List<String> options) {
-        super(questionBody, answer, difficultyLevel, department);
+        super(topic, questionBody, answer, difficultyLevel, department);
         this.options = options;
     }
 
