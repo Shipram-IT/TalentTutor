ef781724eda687c858c29fda759606a0adf4d997
Shivani Sehgal <sehgalshivani07@gmail.com>
2023-12-11 18:54:56 -0500

    Added fill in the blanks question class

--- src/classes/Question/FillInTheBlanksQuestion.java
+++ src/classes/Question/FillInTheBlanksQuestion.java
@@ -1,15 +1,19 @@
-// Concrete class for Fill in the Blanks Question
+package classes.Question;
+
+import enums.Department;
+
+// Concrete class for Fill in the Blanks classes.Question.Question
 public class FillInTheBlanksQuestion extends Question {
     /**
-     * Constructor for FillInTheBlanksQuestion.
+     * Constructor for classes.Question.FillInTheBlanksQuestion.
      *
      * @param questionBody    The body of the question.
      * @param answer          The answer to the question.
      * @param difficultyLevel The difficulty level of the question.
      * @param department      The department associated with the question.
      */
-    public FillInTheBlanksQuestion(String questionBody, String answer, int difficultyLevel, Department department) {
-        super(questionBody, answer, difficultyLevel, department);
+    public FillInTheBlanksQuestion(String topic, String questionBody, String answer, int difficultyLevel, Department department) {
+        super(topic, questionBody, answer, difficultyLevel, department);
     }
 
     /**
