3a16ffb68313ab71753bfcffb161072b8e45a21d
Shivani Sehgal <sehgalshivani07@gmail.com>
2023-12-12 10:30:08 -0500

    Added common utilities interface

---
+++ src/utilities/Common.java
@@ -0,0 +1,29 @@
+package utilities;
+
+import enums.Department;
+
+public interface Common {
+    /**
+     * Takes department as String and returns respective Department enum
+     * @param dept Department String
+     * @return Department Enum
+     */
+    public static Department getDepartment(String dept) {
+        Department department;
+        switch (dept) {
+            case "IT":
+                department = Department.IT;
+                break;
+            case "HR":
+                department = Department.HR;
+                break;
+            case "MARKETING":
+                department = Department.MARKETING;
+                break;
+            default:
+                department = Department.NONE;
+        }
+
+        return department;
+    }
+}
